da801c1010605ebed076abc39c47071c8fa686d8
Ichiro Tiheiroki <74300287+ichirotiheiroki@users.noreply.github.com>
2022-10-05 16:45:06 +0400

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,37 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from "./material/material.module";
+import { ReactiveFormsModule,FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { DatePipe } from '@angular/common';
 
-import {MaterialExampleModule} from '../material.module';
-import {DialogOverviewExample, DialogOverviewExampleDialog} from './dialog-overview-example';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employees/employee/employee.component';
+import { EmployeeService } from './shared/employee.service';
+import { environment } from '../environments/environment';
+import { DepartmentService } from './shared/department.service';
+import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
 
 @NgModule({
-  declarations: [DialogOverviewExample, DialogOverviewExampleDialog],
-  imports: [
-    BrowserAnimationsModule,
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    MatNativeDateModule,
-    MaterialExampleModule,
-    ReactiveFormsModule,
+  declarations: [
+    AppComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    EmployeeListComponent
   ],
-  providers: [],
-  bootstrap: [DialogOverviewExample],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    FormsModule
+  ],
+  providers: [EmployeeService,DepartmentService,DatePipe],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
